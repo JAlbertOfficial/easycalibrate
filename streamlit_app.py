3a17b32c6eeee4cb30e9091657726ab04e1c49d1
JAlbertOfficial <ja.social@tuta.com>
2024-06-10 12:36:45 +0200

    style: Update headers and subheaders for consistency
    Enhance UI by standardizing header levels
    
    - Change all subheaders to headers for main page titles
    - Replace markdown bold text with subheaders for section titles
      to improve visual hierarchy and consistency

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 ################################################################
 # Packages
 ###############################################################
@@ -9,7 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LinearRegression
-
 
 ###############################################################
 # Define page titles
@@ -25,7 +23,7 @@ references = "References"
 ###############################################################
 
 def render_home():
-    st.subheader("Home Page")
+    st.header("Home Page")
     st.write("Welcome to the Home page!")
 
 ###############################################################
@@ -33,19 +31,18 @@ def render_home():
 ###############################################################
 
 def render_basic_calibration():
-    st.subheader("Basic Calibration Page")
-    st.markdown("**Import Data**")    
+    st.header("Basic Calibration Page")
+    st.subheader("Import Data")
 
     # Default values for x and y
-   # Default values for x and y
     default_x_values = ("0.050, 0.050, 0.050, 0.125, 0.125, 0.125, "
-                    "0.500, 0.500, 0.500, 1.250, 1.250, 1.250, "
-                    "2.500, 2.500, 2.500, 5.000, 5.000, 5.000, "
-                    "12.500, 12.500, 12.500, 25.000, 25.000, 25.000")
+                        "0.500, 0.500, 0.500, 1.250, 1.250, 1.250, "
+                        "2.500, 2.500, 2.500, 5.000, 5.000, 5.000, "
+                        "12.500, 12.500, 12.500, 25.000, 25.000, 25.000")
     default_y_values = ("9.102775, 9.102971, 9.096732, 22.658198, 22.882701, 22.830106, "
-                    "77.690938, 75.064287, 80.320072, 197.030149, 197.390646, 196.477779, "
-                    "388.543543, 382.672992, 378.273372, 844.937521, 799.804932, 799.695752, "
-                    "1996.367224, 1987.843702, 1969.842072, 3901.977880, 3786.692867, 3762.291002")
+                        "77.690938, 75.064287, 80.320072, 197.030149, 197.390646, 196.477779, "
+                        "388.543543, 382.672992, 378.273372, 844.937521, 799.804932, 799.695752, "
+                        "1996.367224, 1987.843702, 1969.842072, 3901.977880, 3786.692867, 3762.291002")
 
     # User inputs
     x_input = st.text_area("Enter x values (comma separated)", default_x_values)
@@ -61,7 +58,7 @@ def render_basic_calibration():
             # Create a dataframe
             data = {'x': x_values, 'y': y_values}
             df = pd.DataFrame(data)
-            st.markdown("**View raw data**")
+            st.subheader("View raw data")
             st.dataframe(df)
             
             # Perform linear regression
@@ -72,13 +69,12 @@ def render_basic_calibration():
             y_pred = model.predict(X)
             
             # Plot the data and the regression line
-            st.markdown("**Calibration plot**")
+            st.subheader("Calibration plot")
             fig, ax = plt.subplots()
             ax.plot(df['x'], df['y'], 'o', label='Data points')
             ax.plot(df['x'], y_pred, '-', label='Regression line')
             ax.set_xlabel('x')
             ax.set_ylabel('y')
-            #ax.set_title('Scatter plot of x vs y with regression line')
             ax.legend()
             
             # Display the plot in Streamlit
@@ -86,13 +82,12 @@ def render_basic_calibration():
         else:
             st.error("The number of x values must be equal to the number of y values.")
 
-
 ###############################################################
 # Function to render improved calibration page
 ###############################################################
 
 def render_improved_calibration():
-    st.subheader("Improved Calibration Page")
+    st.header("Improved Calibration Page")
     st.write("This is the Improved Calibration page!")
 
 ###############################################################
@@ -100,7 +95,7 @@ def render_improved_calibration():
 ###############################################################
 
 def render_references():
-    st.subheader("References Page")
+    st.header("References Page")
     st.write("This is the References page!")
 
 ###############################################################
